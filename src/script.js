964b0b4a05327b484cd542b14825cbeddee3a82f
Giannina Miñano <g.isabella.md@gmail.com>
2022-03-03 20:15:38 -0300

    First commit

--- src/script.js
+++ src/script.js
@@ -2,6 +2,11 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { Color, Plane } from 'three'
+
+// Texture Loader
+const loader = new THREE.TextureLoader()
+const cross = loader.load('./cross.png')
 
 // Debug
 const gui = new dat.GUI()
@@ -15,14 +20,40 @@ const scene = new THREE.Scene()
 // Objects
 const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
+const particlesGeometry = new THREE.BufferGeometry;
+const particlesCnt = 5000;
+
+const posArray = new Float32Array(particlesCnt * 3);
+// xyz, xyz, xyz
+
+for (let i = 0; i < particlesCnt * 3; i++) {
+    // posArray[i] = Math.random();
+    // posArray[i] = (Math.random() - 0.5) * 5;
+    posArray[i] = (Math.random() - 0.5) * (Math.random() * 5);
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray,3))
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// const material = new THREE.MeshBasicMaterial()
+// material.color = new THREE.Color(0xff0000)
+const material = new THREE.PointsMaterial({
+    size: 0.005
+})
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.005,
+    map: cross,
+    transparent: true,
+    // color: 'yellow'
+    
+})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+// const sphere = new THREE.Mesh(geometry,material)
+const sphere = new THREE.Points(geometry,material)
+const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(sphere, particlesMesh)
 
 // Lights
 
@@ -77,6 +108,20 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color('#21282a'), 1)
+
+// Mouse
+
+document.addEventListener('mousemove', animateParticles)
+
+let mouseX = 0
+let mouseY = 0
+
+function animateParticles(event) {
+    mouseY = event.clientY
+    mouseX = event.clientX
+}
+
 
 /**
  * Animate
@@ -91,7 +136,11 @@ const tick = () =>
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
+    particlesMesh.rotation.y = -.1 * elapsedTime
 
+    if (mouseX > 0) {particlesMesh.rotation.x =  -mouseY * (elapsedTime *0.00008)
+    particlesMesh.rotation.y =  -mouseX * (elapsedTime *0.00008)}
+    
     // Update Orbital Controls
     // controls.update()
 
